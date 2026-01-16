ace4b4c6b28fcf5f36bf8d12d54e6c5b9b950dd5
Chinmay Mahto <166236363+chinmayyy01@users.noreply.github.com>
2026-01-16 07:00:12 +0530

    Parse HTTP request line (method, path, version)

--- src/main/java/com/server/ClientHandler.java
+++ src/main/java/com/server/ClientHandler.java
@@ -12,14 +12,23 @@ public class ClientHandler {
 
     public void handle() throws Exception {
         InputStream inputStream = socket.getInputStream();
+        
+        byte[] buffer = new byte[4096];
+        int bytesRead = inputStream.read(buffer);
+        String raw = new String(buffer, 0 , bytesRead);
 
-        byte[] buffer = new byte[1204];
-        int bytesRead;
+        String[] lines = raw.split("\r\n");
 
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            String data = new String(buffer, 0, bytesRead);
-            System.out.println(data);
-        }
+        String requestLine = lines[0];
+
+        String[] parts = requestLine.split(" ");
+        String method = parts[0];
+        String path = parts[1];
+        String version = parts[2];
+
+        System.out.println("Method: " + method);
+        System.out.println("Path: " + path);
+        System.out.println("Version: " + version);
 
         inputStream.close();
         socket.close();
